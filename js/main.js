cde288718980768fcbb22dedc7610b672522f338
MaximParusimov <97607663+MaximParusimov@users.noreply.github.com>
2022-10-14 13:52:45 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -8,11 +8,47 @@ function getRandomInteger(min, max) {
   }
   return Math.floor(randomNamber);
 }
-getRandomInteger(1, 20);
-
 
 const MAX_LENGTH = 140;
 
 const checkStringLengths = (string) => (string.length <= MAX_LENGTH);
 
 checkStringLengths('string');
+
+
+const DESCRIPTION = [
+  'Лето',
+  'Осень',
+  'Зима',
+  'Весна',
+  'Пора уезжать',
+  'Я остаюсь',
+  'Бывало и лучше',
+  'Раньше было лучше',
+  'Все хорошо',
+  'Закат',
+  'Я не люблю скайрим',
+  'Просто космос',
+  'Библейская история',
+  'Игорь существует',
+];
+
+const DESCRIPTION_FOTOS = 25;
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const fotoDescription = (getId) => ({
+  id: getId,
+  url: `photos/${getRandomInteger(1,25)}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomInteger(15, 200),
+  comments: getRandomInteger(0, 200),
+});
+
+const similarDescriptionPhoto = Array.from({
+  length: DESCRIPTION_FOTOS
+}, (a, b) => fotoDescription(b));
+
+export {
+  similarDescriptionPhoto,
+};
